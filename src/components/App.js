de7ed6aee54490e1f400465188b661902da5d6be
jpickard393 <jpickard@and.digital>
2022-01-04 19:29:45 +0000

    section 3 style random pay numbers

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,11 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import Game from './Game';
 class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>TargetSun...</Text>
-            </View>
+            <Game randomNumberCount={6} />
         );
     }
 }
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#ddd', flex: 1,
-    },
-});
+
 export default App;
